45cdcb45efef2b67e1259de8ff52bb5c3452f4eb
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2019-08-31 12:29:20 +0530

    Test 1

---
+++ precache-manifest.ffe98f5d04e650d28ab62a3a24c976cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749322b53d63df589714a55d48d58e5c",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "7b912244239146e14e10",
+    "url": "/myapp/static/css/main.89018614.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/myapp/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "7b912244239146e14e10",
+    "url": "/myapp/static/js/main.ab906958.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "b994aedd36f41c6cdfbd2802603d479f",
+    "url": "/myapp/static/media/chatbot.b994aedd.png"
+  },
+  {
+    "revision": "d1ee13d34ff786a7c762f4f2112fb717",
+    "url": "/myapp/static/media/itme.d1ee13d3.jpg"
+  }
+]);
